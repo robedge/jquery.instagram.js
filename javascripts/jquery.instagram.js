947d63fe82e0343aa34190f1335478938af97742
Rob Edgell <redgell@gmail.com>
2011-06-18 19:34:36 -0500

    Added methods for searching by tag name and for getting the most popular images

--- javascripts/jquery.instagram.js
+++ javascripts/jquery.instagram.js
@@ -1,6 +1,6 @@
 (function ($) {
     var settings = {
-        authToken: "332178.5532830.304dcae9620749e086bac69712af2051", // replace with your token or pass a new value in the settings
+        authToken: "", // replace with your token or pass a new value in the settings
         loadingMessage: "Finding and loading instagram stream...", // This can be any html you want to show for loading
         count: 10
     }
@@ -54,7 +54,38 @@
                     'https://api.instagram.com/v1/users/self/media/liked?count=' + settings.count + '&access_token=' + settings.authToken
                 );
             });
-        }
+        },
+        /**
+          * Get the current most popular images
+          */
+        getPopularStream: function(options) {
+            if (options) {
+                $.extend(settings, options);
+            }
+            
+            var $e = this;
+            initLoad($e, function() {
+                getInstagramFeed(
+                    $e,
+                    settings,
+                    'https://api.instagram.com/v1/media/popular?count=' + settings.count + '&access_token=' + settings.authToken
+                );
+            });
+        },
+        getStreamByTag: function(options) {
+            if (options) {
+                $.extend(settings, options);
+            }
+            
+            var $e = this;
+            initLoad($e, function() {
+                getInstagramFeed(
+                    $e,
+                    settings,
+                    'https://api.instagram.com/v1/tags/' + settings.tag + '/media/recent?count=' + settings.count + '&access_token=' + settings.authToken
+                );
+            });
+        },
     };
 
     function initLoad(e, callback) {
